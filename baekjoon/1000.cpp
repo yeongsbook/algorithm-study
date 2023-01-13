603f68db79213a46d1a76ffcf19123ba42ccc6c7
yeongsbook <yeongsbook@gmail.com>
2023-01-13 13:50:23 +0900

    baekjoon-1000

---
+++ baekjoon/1000.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/1000
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    int A;
+    int B;
+
+    scanf("%d %d", &A, &B);
+
+    printf("%d", A + B);
+    return 0;
+}
